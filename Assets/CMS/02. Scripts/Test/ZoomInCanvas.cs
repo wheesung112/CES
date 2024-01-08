6304c687782e1dde71b9b56c2657597326ceea77
inbear-evan <61470680+inbear-evan@users.noreply.github.com>
2024-01-09 06:20:16 +0900

    e
    e

--- Assets/CMS/02. Scripts/Test/ZoomInCanvas.cs
+++ Assets/CMS/02. Scripts/Test/ZoomInCanvas.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class ZoomInCanvas : MonoBehaviour
 {
-    public int maxSize = 2;
+    public int maxSize = 4;
     public float steps = 0.001f;
-    public Vector3 defaultSize = new Vector3(0.022624f, 1, 0.032f);
+    Vector3 defaultSize;
     Vector3 currentSize;
     Vector3 vmaxSize;
     Vector3 vminSize;
     // Start is called before the first frame update
     void Start()
     {
+        defaultSize = transform.parent.localScale;
         vminSize = defaultSize / 2;
         vmaxSize = defaultSize * 2;
         currentSize = transform.parent.localScale;
